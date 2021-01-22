ac1a8fe5f35789f5a523ad05269e36119904d8a7
wcascades <wildcascades@yahoo.com>
2021-01-22 13:01:14 -0800

    Added from sender check. Left in a testing measure  Changes to be committed: 	modified:   slackhandler.py

--- slackhandler.py
+++ slackhandler.py
@@ -7,15 +7,25 @@ TOKEN = mysecrets.slack_token
 DEFAULT_CHANNEL = mysecrets.default_slack_channel
 
 
+def _get_from_address(str):
+    return str[str.find("<")+1:str.find(">")]
+
+
 def notify(mail, channel=DEFAULT_CHANNEL):
+    if(mail["From"] == mysecrets.ticket_system_email_address):
+        message_for_slack = mail['Subject'] + " <!here>"
+    else:
+        # todo: for testing purposes
+        message_for_slack = mail['Subject']
+
     try:
         client = WebClient(token=TOKEN)
-        response = client.chat_postMessage(channel=channel, text=mail['Subject'])
-        assert response["message"]["text"] == mail['Subject']
+        response = client.chat_postMessage(channel=channel, text=message_for_slack)
+        assert response["message"]["text"] == message_for_slack
     except SlackApiError as e:
         logging.debug("slackhandler.py:: " + f"Response 'ok' : {e.response['ok']}")
         logging.debug("slackhandler.py:: " + "ERROR: " + e.response["error"])
     except AssertionError as e:
         logging.debug("slackhandler.py:: ASSERTION ERROR")
 
-    logging.debug("slackhandler.py:: " + "message supplied is " + mail['Subject'])
+    logging.debug("slackhandler.py:: " + "message supplied is " + message_for_slack)
